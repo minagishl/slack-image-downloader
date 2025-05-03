2723d1975bfb879e0114640641555c85504322b5
minagishl <mvxd568.co@icloud.com>
2025-05-03 22:02:37 +0900

    refactor: change URL validation method

--- src/background.ts
+++ src/background.ts
@@ -4,12 +4,19 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // Enable access to Slack when the extension is in use
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.id && tab.url?.includes("slack.com")) {
-    await chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        console.log("Slack Image Downloader extension activated");
-      },
-    });
+  if (tab.id && tab.url) {
+    try {
+      const url = new URL(tab.url);
+      if (url.hostname.endsWith("slack.com")) {
+        await chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          func: () => {
+            console.log("Slack Image Downloader extension activated");
+          },
+        });
+      }
+    } catch (e) {
+      console.error("Invalid URL:", e);
+    }
   }
 });
